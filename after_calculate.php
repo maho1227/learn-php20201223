34aba6135abb65369568a2a2ecd3639e3e3f67d7
maho1227 <mahomadoka1227@gmail.com>
2020-12-29 01:16:40 +0900

    計算後の画面表示を修正

--- after_calculate.php
+++ after_calculate.php
@@ -6,7 +6,16 @@
     $radius = $_GET["radius"]; 
     //var_dump ($radius);
     $circle_area = calculate_circle($radius);
-    echo "半径が{$radius}cmの円の面積は{$circle_area}㎠です";
-    echo nl2br("\n");
     //var_dump($circle_area); 
 ?>	
+<!doctype html>
+    <html lang-"ja">
+    <head>
+        <meta charset-"UTF-8">
+        <title>計算後</title>
+    </head>
+    <body>
+        <div class-"main>
+        <h2><?php echo "半径が{$radius}cmの円の面積は{$circle_area}㎠です";?></h2>
+    </body>
+    </html>
--- before_calculate.php
+++ before_calculate.php
@@ -1,20 +1,20 @@
-<!doctype html>
-<html lang-"ja">
-<head>
-    <meta charset-"UTF-8">
-    <title>PHPテスト</title>
-</head>
-<body>
-    <div class-"main>
-    <h1>円の面積計算プログラム</h1>
-    <h2>半径を入力してください。</h2>
-    <form action="after_calculate.php" method="get">
-        <div>
-            <lavel for="radius">半径</lavel>
-            <input type ="text" id ="radius" name = "radius">
-            <lavel for="length">cm</lavel>
-            <input type = "submit" value ="計算する">
-     </form>
-    </div>
-</body>
-</html> 
+    <!doctype html>
+    <html lang-"ja">
+    <head>
+        <meta charset-"UTF-8">
+        <title>PHPテスト</title>
+    </head>
+    <body>
+        <div class-"main>
+        <h1>円の面積計算プログラム</h1>
+        <h2>半径を入力してください。</h2>
+        <form action="after_calculate.php" method="get">
+            <div>
+                <lavel for="radius">半径</lavel>
+                <input type ="text" id ="radius" name = "radius">
+                <lavel for="length">cm</lavel>
+                <input type = "submit" value ="計算する">
+        </form>
+        </div>
+    </body>
+    </html> 
