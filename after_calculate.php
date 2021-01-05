3565dabfb632e98facb8e149858d859ede107801
maho1227 <mahomadoka1227@gmail.com>
2021-01-06 04:24:41 +0900

    バリデーション追加

--- after_calculate.php
+++ after_calculate.php
@@ -1,12 +1,14 @@
 <?php
-    function calculate_circle($radius) {
-        define("PI",3.141592);
+    //円周率を3.14に設定
+    define("PI",3.14);
+    //POST値radiusを取得し、int型にキャスト
+    $radius = (int) $_POST["radius"];
+    
+    //POST値radiusを元に円の面積を計算する関数
+    function calculate_circle($radius) {   
         return $radius * $radius * PI; 
     }
-    $radius = $_GET["radius"]; 
-    //var_dump ($radius);
-    $circle_area = calculate_circle($radius);
-    //var_dump($circle_area); 
+    $circle_area = calculate_circle($radius); 
 ?>	
 <!doctype html>
     <html lang-"ja">
@@ -15,7 +17,8 @@
         <title>計算後</title>
     </head>
     <body>
-        <div class-"main>
-        <h2><?php echo "半径が{$radius}cmの円の面積は{$circle_area}㎠です";?></h2>
+        <div class-"main">
+        <h1>円の面積計算プログラム</h1>
+        <h2><?php echo "半径が{$radius}cmの円の面積は{$circle_area}㎠です";?></h2>    
     </body>
     </html>
--- before_calculate.php
+++ before_calculate.php
@@ -1,20 +1,32 @@
-    <!doctype html>
+<!doctype html>
     <html lang-"ja">
     <head>
         <meta charset-"UTF-8">
-        <title>PHPテスト</title>
+        <title>円の面積計算プログラム</title>
+        <script type="text/javascript">
+            function check() {
+                let val = document.getElementById('radius');
+                if (radius.radius.value =="") {
+                    alert("※半径を入力してください。");
+                    return false;
+                }else if (radius.radius.value.match(/[^0-9]+/)) {
+                    alert("半角数字で入力してください。");
+                    return false;
+                }   
+                    return true;
+                }
+        </script>
     </head>
     <body>
         <div class-"main>
         <h1>円の面積計算プログラム</h1>
-        <h2>半径を入力してください。</h2>
-        <form action="after_calculate.php" method="get">
-            <div>
+        <h2>半径を入力してください。(円周率は3.14で統一しています)</h2>
+            <form action="after_calculate.php" method="post" name ="radius">
                 <lavel for="radius">半径</lavel>
                 <input type ="text" id ="radius" name = "radius">
                 <lavel for="length">cm</lavel>
-                <input type = "submit" value ="計算する">
-        </form>
+                <input type = "submit" id ="calculate" name = "calculate" value ="計算する" onClick="return check();"><br>
+            </form>
         </div>
     </body>
     </html> 
